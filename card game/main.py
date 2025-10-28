10b859361ab2937ba4c35b10fceda3a70261e204
nazar-zaman <kazmin@mercia.school>
2025-10-28 19:41:10 +0000

    saving progress

--- card game/main.py
+++ card game/main.py
@@ -7,35 +7,29 @@ screen_title = "Stage 2 Example"
 class MyGame(arcade.Window):
     def __init__(self):
         super().__init__(screen_width, screen_height, screen_title)
-        arcade.set_background_color(arcade.color.MAHOGANY)
+        arcade.set_background_color(arcade.color.WHITE)
+        
         self.x = 100
         self.y = 100
-        self.speed_x = 0
-        self.speed_y = 0
-    
-    def on_draw(self):
-        self.clear()
-        arcade.draw_circle_filled(self.x,self.y,20,arcade.color.RED)
+        self.circle_r = 40
+        self.circle_vis = True
+        self.score = 0
+        self.score_text = arcade.Text(f"Score: {self.score}", 10, 570, color=arcade.color.BLACK_LEATHER_JACKET)
+        
+        def on_draw(self):
+            self.clear()
+            if self.circle_vis:
+                arcade.draw_circle_filled(self.x, self.y, self.circle_radius, arcade.color.MAHOGANY)
+        
+        def on_mouse_press(self, x, y, button, modifiers):
+            dx = x - self.x
+            dy = y - self.y
+            if dx * dx + dy * dy <= self.circle_radius **2:
+                self.circle_vis = False
+                self.score += 1
 
-    def on_update(self, delta_time):
-        self.x += self.speed_x
-        self.y += self.speed_y
-
-    def on_key_press(self, key, modifiers):
-        if key == arcade.key.RIGHT:
-            self.speed_x += 10
-        if key == arcade.key.LEFT:
-            self.speed_x -= 10
-        if key == arcade.key.UP:
-            self.speed_y += 10
-        if key == arcade.key.DOWN:
-            self.speed_y -= 10
-
-    def on_key_release(self, key, modifiers):
-        if key in [arcade.key.LEFT, arcade.key.RIGHT]:
-            self.speed_x = 0
-        elif key in [arcade.key.DOWN, arcade.key.UP]:
-            self.speed_y = 0
+        def on_update(self, delta_time):
+            self.score_text.text = f"Score: {self.score}"
 
 def main():
     game = MyGame()
